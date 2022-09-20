6ef18036eb82754d0e13ec498d85adc40935362e
Hassan Harera <Hassan.Shaban.Harera@gmail.com>
2022-09-20 11:47:36 +0200

    reformat code

--- src/main/java/com/clinico/world/WorldApplication.java
+++ src/main/java/com/clinico/world/WorldApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WorldApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WorldApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WorldApplication.class, args);
+    }
 
 }
